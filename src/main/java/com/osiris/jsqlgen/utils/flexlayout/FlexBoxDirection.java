f31da433004279991f442f11cfece61c9ccca1f1
Osiris Team <osiris_support@pm.me>
2022-07-25 20:04:26 +0200

    support for bigger windows

---
+++ src/main/java/com/osiris/jsqlgen/utils/flexlayout/FlexBoxDirection.java
@@ -0,0 +1,12 @@
+package com.osiris.jsqlgen.utils.flexlayout;
+
+/**
+ * Created by TB on 12.10.16.
+ */
+public enum FlexBoxDirection
+{
+    ROW(),
+    ROW_REVERSE(),
+    COLUMN(),
+    COLUMN_REVERSE();
+}
