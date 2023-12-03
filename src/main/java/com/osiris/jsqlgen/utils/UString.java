004c68ca828d2c8e6f7cc3235b38cbe415b3321a
Osiris Team <osiris_support@pm.me>
2023-12-03 18:25:27 +0100

    1.31 - FIX: enum support causing issue with sql functions and thus failing to find definition/type

--- src/main/java/com/osiris/jsqlgen/utils/UString.java
+++ src/main/java/com/osiris/jsqlgen/utils/UString.java
@@ -2,13 +2,15 @@ package com.osiris.jsqlgen.utils;
 
 public class UString {
     public static boolean startsWithWordIgnoreCase(String s, String query) {
-        String firstWord;
-        if(s.contains("(")) firstWord = s.substring(0, s.indexOf("(")); // Support enums
-        else firstWord = s.split(" ")[0];
-
+        String firstWord = s.split(" ")[0];
         firstWord = firstWord.trim();
 
-        return firstWord.equalsIgnoreCase(query);
+        if(firstWord.equalsIgnoreCase(query)) return true;
+        else {
+            // Support enums
+            if(s.contains("(")) firstWord = s.substring(0, s.indexOf("("));
+            return firstWord.equalsIgnoreCase(query);
+        }
     }
 
     public static boolean containsIgnoreCase(String s, String query) {
